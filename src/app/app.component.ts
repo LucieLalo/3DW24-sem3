a9cb6f7911846f3c807b28c94934a5f47b67e54d
Lalo Lucie <lucie.lalo@etu.unilim.fr>
2019-12-22 18:29:14 +0100

    Site's skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './content-body/content-body.component.html',
+  styleUrls: ['./content-body/content-body.component.scss']
 })
 export class AppComponent {
   title = 'game-tracker';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentBodyComponent } from './content-body/content-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentBodyComponent
   ],
   imports: [
     BrowserModule,
